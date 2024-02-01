f58d4cf2a9aa3bb7b166f7976aa5a12e3403bc2e
Maxim Mineev <mms1401@ya.ru>
2024-02-01 11:32:06 +0500

    states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
+    const [likes, setLikes] = useState(0);
+    const [value, setValue] = useState('input text')
 
-    </div>
-  );
+    const increment = () => {
+        setLikes(likes + 1);
+    }
+
+    const decrement = () => {
+        setLikes(likes - 1)
+    }
+
+    return (
+        <div className="App">
+            <h1>{likes}</h1>
+            <h1>{value}</h1>
+            <input
+                type="text"
+                value={value}
+                onChange={event => setValue(event.target.value)}
+            />
+            <button onClick={increment}>
+                Increment
+            </button>
+            <button onClick={decrement}>
+                Decrement
+            </button>
+        </div>
+    );
 }
 
 export default App;
